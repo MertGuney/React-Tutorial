f406f6a7e2c58bcc072d0ee58a9710c9308f77ab
Mert Güney <mert_guney_29@hotmail.com>
2022-06-26 01:11:28 +0300

    Props

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
-import './App.css';
-import User from './components/User';
-import Navbar from './components/Navbar';
+import "./App.css";
+import User from "./components/User";
+import Navbar from "./components/Navbar";
 
 function App() {
   const test = 35;
   const isAuth = true;
   return (
-    <div className='container'>
-      <Navbar/>
-      <h1 style={{color:"red",fontSize:"100px"}}>Merhaba</h1>
-      <h1>1+1 Toplamı:{1+1}</h1>
-      <h4 className='header'>{"mert".toUpperCase()}</h4>
+    <div className="container">
+      <Navbar title="User App" />
+      <hr />
+      <h1 style={{ color: "red", fontSize: "100px" }}>Merhaba</h1>
+      <h1>1+1 Toplamı:{1 + 1}</h1>
+      <h4 className="header">{"mert".toUpperCase()}</h4>
       <h4>{test}</h4>
       <div>
-        {isAuth?<p> Kullanıcı kayıtlı</p>:<p>Kullanıcı kayıtlı değil</p>}
+        {isAuth ? <p> Kullanıcı kayıtlı</p> : <p>Kullanıcı kayıtlı değil</p>}
       </div>
       <div>
         <h4>User Component</h4>
-        <User/>
+        <User name="Mert" salary="5000" department="Yazilim" />
+        <User />
       </div>
     </div>
   );
--- src/components/User.js
+++ src/components/User.js
@@ -1,15 +1,47 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import PropTypes from "prop-types";
 
 class User extends Component {
+  static defaultProps = {
+    name: "Bilgi Yok",
+    salary: "0",
+    department: "Bilgi Yok",
+  };
   render() {
+    // destructing
+    const { name, department, salary } = this.props;
     return (
-      <div>
-        <form>
-            <input type="text"/>
-            <button>Gönder</button>
-        </form>
+      <div className="col-md-8 mb-4">
+        <div className="card">
+         <div className="card-header d-flex justify-content-between ">
+          <h4 className="d-inline">{name}</h4><i className="far fa-trash-alt" style={{cursor:"pointer"}}></i>
+         </div>
+         <div className="card-body">
+          <p className="card-text"> Maaş: {salary}</p>
+          <p className="card-text"> Department: {department}</p>
+         </div>
+        </div>
+        {/* <ul>
+          <li>Isim : {name} <i className="far fa-trash-alt"></i></li>
+          <li>Departman : {department}</li>
+          <li>Maas : {salary}</li>
+        </ul> */}
       </div>
-    )
+    );
+    // return (
+    //   <div>
+    //     <ul>
+    //       <li>Isim :  {this.props.name}</li>
+    //       <li>Departman : {this.props.department}</li>
+    //       <li>Maas : {this.props.salary}</li>
+    //     </ul>
+    //   </div>
+    // )
   }
 }
+User.propTypes = {
+  name: PropTypes.string.isRequired,
+  salary: PropTypes.string.isRequired,
+  department: PropTypes.string.isRequired,
+};
 export default User;
